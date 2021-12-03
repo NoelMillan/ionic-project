f75da6dd7e7df9449f6610b530e1d5c169397879
noelm <noelmillan@securekids.es>
2021-12-03 02:23:14 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/center/center.page.ts
+++ src/app/pages/center/center.page.ts
@@ -2,15 +2,39 @@ import { LocationModalComponent } from './../../components/location-modal/locati
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { NavigationService } from 'src/app/services/navigation.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-center',
   templateUrl: './center.page.html',
   styleUrls: ['./center.page.scss'],
+    animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':leave', 
+          [
+            style({opacity: 1 }),
+            animate('.2s ease-out', 
+                    style({opacity: 0 }))
+          ]
+        ),
+        transition(
+          ':enter', 
+          [
+            style({opacity: 0 }),
+            animate('.5s ease-out', 
+                    style({opacity: 1 }))
+          ]
+        ),
+      ]
+    ),
+  ]
 })
 export class CenterPage implements OnInit {
 
-  fondo: boolean = true;
+  centerPressed: boolean = false;
 
   option = {
     slidesPerView: 1.5,
